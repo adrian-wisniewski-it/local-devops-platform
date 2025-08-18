dd57e2f0f6aab6b1c426213779942ff9c7aa3f01
adrian-wisniewski-it <adrian.wisniewski.it@gmail.com>
2025-08-18 23:14:19 +0000

    Update the Flask application by adding /readyz and /healthz endpoints, configure Kubernetes readiness and liveness probes to listen to those endpoints, set container resource limits for CPU and RAM usage

--- app/app.py
+++ app/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return "Hello DevOps"
 
+@app.get("/healthz")
+def healthz():
+    return "", 204
+
+@app.get("/readyz")
+def readyz():
+    return "", 204
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
